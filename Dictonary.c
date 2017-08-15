ac28e2f708ec3778fa90cc4ae3f75c2ee6578d91
Kshreyans <ks.shreyanshu31@gmail.com>
2017-08-15 21:26:38 +0530

    through malloc

--- Dictonary.c
+++ Dictonary.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 
-void merge(char word[][10], int min, int mid, int max){
+void merge(char **word, int min, int mid, int max){
 
      int i=min,j=mid+1,k=0;   
 
@@ -27,11 +28,12 @@ void merge(char word[][10], int min, int mid, int max){
         strcpy(word[min+i],a[i]);
 }
 
-void merge_Sort(char word[][10], int min,int max){
+void merge_Sort(char **word, int min,int max){
 
    int mid;
    
    if(min<max){
+     printf("1st\n");
      mid=(min+max)/2;
      merge_Sort(word,min,mid);
      merge_Sort(word,mid+1,max);    
@@ -39,9 +41,9 @@ void merge_Sort(char word[][10], int min,int max){
    }
 }
 
-void binary_search(char word[][10], int min,int max){
+void binary_search(char **word, int min,int max){
 
-char find[10];
+char find[20];
     scanf("%s",find); 
      
      int mid;
@@ -69,15 +71,16 @@ int main(){
    int min=0,max;
    int n,i;
    printf("Enter the no. of words:-");scanf("%d",&n);
-   max=n;
-    char word[n][10];
+   max=n-1;
+    char **word=(char **)malloc(sizeof(char *)*n);
    for(i=0;i<n;i++)
    {    getchar();
+        word[i]=(char *)malloc(sizeof(char)*20);
         scanf("%s",word[i]);
    }
- 
+ printf("words entered\n");
   merge_Sort(word,min,max);
-  
+  printf("merge sorted\n");
   binary_search(word,min,max); 
    return 0;
 }
