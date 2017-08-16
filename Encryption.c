558bd07e7656f8ba644447a71e63c7177c8f2cdf
Kshreyans <ks.shreyanshu31@gmail.com>
2017-08-16 22:58:36 +0530

    Encryption

---
+++ Encryption.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+
+int main(){
+
+    double len,col=0;
+    int i,j;
+    
+    char *a=NULL;
+    a=(char *)malloc(sizeof(char)*100);
+
+    scanf("%s",a);
+    len=(double)strlen(a);
+    col=ceil(sqrt(len));
+    
+    for( i=0; i<col; i++){
+    
+        for( j=i; j<len; j=j+col)
+          printf("%c",a[j]);
+    
+      printf(" ");
+    }
+     return 0;
+}
