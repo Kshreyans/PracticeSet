6df66c4595f198e967f03f94257d99eb1db3c2a3
Kshreyans <ks.shreyanshu31@gmail.com>
2017-10-17 10:04:18 +0530

    Update part

--- AbsolutePermutation.c
+++ AbsolutePermutation.c
@@ -1,34 +1,45 @@
 //
 
 #include<stdio.h>
+#include<string.h>
 
 int main(){
-    int t,i; 
+    int t; 
     scanf("%d",&t);
     for(int a0 = 0; a0 < t; a0++){
-        int n; 
-        int k; 
-        scanf("%d %d",&n,&k);
-        
+        long n; 
+        long k,i; 
+        scanf("%ld %ld",&n,&k);
+        int b[n+1],f=0;
+        bzero(b,n);
         if(k==0)
         {
                 for(i=1; i<=n; i++)
-                 printf("%d ",i);
+                 printf("%ld ",i);
         }
         else
-        { int b[n+1],l,r;
+        { long l,r;
                 for(i=1; i<=n; i++)
                 {       
                       l=i-k;
                       r=i+k;
-                      if(i>k && b[l-2]!=l)
+                      if(i>k && b[l-k]!=l)
                         b[i]=l;
                       else  
                         b[i]=r;
-                    printf("%d ",b[i]);    
+                    if(b[i]>n || b[i]<=0)
+                     f=1;  
+                
                 }
+                printf("done   i=%ld\n",i);
+                if(f==0|| k!=0)
+                  for(i=1; i<=n; i++)
+                   printf("%d ",b[i]);
+                  else
+                     printf("-1"); 
         }
-
+       // printf("++++++++ n=%d\n",n);
+              
         printf("\n");
     }
     return 0;
