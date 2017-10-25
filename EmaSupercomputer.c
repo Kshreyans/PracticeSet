578962eb55642356296967f1754f95dccab3ec73
Kshreyans <ks.shreyanshu31@gmail.com>
2017-10-25 21:08:24 +0530

    2d Array conditional geometery part 2 trial

--- EmaSupercomputer.c
+++ EmaSupercomputer.c
@@ -2,6 +2,14 @@
 
 #include<stdio.h>
 
+struct node{
+        
+        int t[2],
+            d[2],
+            l[2],
+            r[2];
+};
+
 int check(int m,char a[][m],int nx,int ny,int px,int py,int i, int j){
         
          
@@ -12,11 +20,34 @@ int check(int m,char a[][m],int nx,int ny,int px,int py,int i, int j){
             {  printf("yea====0\n");  return 0;  }     
 }
 
+int overlap(struct node vpos, struct node maxpos){
+
+        if(vpos.r[0]==maxpos.l[0] && vpos.r[1]>=maxpos.l[1])
+          return 1;
+        
+        if(vpos.t[1]==maxpos.d[1] && vpos.t[0]>=maxpos.d[0])
+         return 1;
+         
+         if( ( vpos.t[0]<=maxpos.r[0] && vpos.t[1]<=maxpos.r[1])  && ( vpos.l[0]<=maxpos.d[0] && vpos.l[1]<=maxpos.d[1] ) )
+         return 1;
+
+         if(   (vpos.r[0]<=maxpos.d[0]   &&  vpos.r[1]>=maxpos.d[1]) && (vpos.t[0]<=maxpos.l[0]  && vpos.t[1]>= maxpos.l[1]) )
+                return 1;
+
+  return 0;
+}               
+
+
+
 int main(){
 
         int n,m,i,j,k=0,v,max1,max2;
         max1=-8;max2=-4;
         
+        struct node vpos,max1pos;
+        vpos.t[0]=vpos.t[1]=vpos.d[0]=vpos.d[1]=vpos.l[0]=vpos.l[1]=vpos.r[0]=vpos.r[1]=0;
+        max1pos.t[0]=max1pos.t[1]=max1pos.d[0]=max1pos.d[1]=max1pos.l[0]=max1pos.l[1]=max1pos.r[0]=max1pos.r[1]=-1;
+              
         scanf("%d%d",&n,&m);
         char a[n][m];
         
@@ -47,12 +78,30 @@ int main(){
                                  break;
                                     
                                  v=v+4;
+                                   vpos.t[0]=px;
+                                   vpos.t[1]=j;
+                                   vpos.d[0]=nx;
+                                   vpos.d[1]=j;
+                                   vpos.l[0]=i;
+                                   vpos.l[1]=py;
+                                   vpos.r[0]=i;
+                                   vpos.r[1]=ny;
                         }
                         
                     }        if(v>max2){
+                                        
+                                      if(!overlap(vpos,max1pos))  
                                         if(v>max1)
                                         { max2=max1;
                                           max1=v;
+                                    max1pos.t[0]=  vpos.t[0];
+                                    max1pos.t[1]=  vpos.t[1];
+                                    max1pos.d[0]=  vpos.d[0];
+                                    max1pos.d[1]=  vpos.d[1];
+                                    max1pos.l[0]=  vpos.l[0];
+                                    max1pos.l[1]=  vpos.l[1];
+                                    max1pos.r[0]=  vpos.r[0];
+                                    max1pos.r[1]=  vpos.r[1];
                                         }
                                         else
                                           max2=v;
